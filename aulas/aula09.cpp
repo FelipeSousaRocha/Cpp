b235c49b7cdb9f080c08d691a9ad41204d4ad5ad
FelipeSRocha <felipebsoim@gmail.com>
2021-01-22 18:47:53 +0000

    aula09

---
+++ aulas/aula09.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  /*/se(teste lógico){
+    se o teste for verdadeiro
+  }
+  se não / caso contrário{
+    se o teste for falso
+  }
+  */
+
+  //teste lógico
+  // >, <, ==, >=, <=, !=
+
+  int num = 1;
+  int sol = 1;
+
+  //if(num >= 10){
+  //  cout << "Resultado VERDADEIRO ";
+  //}
+
+  if(sol){
+    cout << "Resultado VERDADEIRO ";
+  }
+  else{
+    cout << "Resultado FALSO";
+  }
+
+ return 0;
+
+}
