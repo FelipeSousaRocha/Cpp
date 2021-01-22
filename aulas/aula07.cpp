9dece545700a5909d8ed6bcb17547f03359e1f5b
FelipeSRocha <felipebsoim@gmail.com>
2021-01-22 18:25:39 +0000

    aula07

---
+++ aulas/aula07.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int n1 = 10;
+
+
+  //n1 = n1+1;
+  //n1+=1;
+  //n1++; ++n1;
+  //n1*=2;
+
+
+  cout << ++n1 << "\n\n";
+
+  return 0;
+}
