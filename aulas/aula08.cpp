97ff05d5425738d0a2905b3440c971e842f23272
FelipeSRocha <felipebsoim@gmail.com>
2021-01-22 18:33:39 +0000

    aula08

---
+++ aulas/aula08.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int num=10;
+
+  cout << num << "\n\n";
+
+  //num = num * -1;
+  //cout << -num << "\n\n";
+  //num=-10;
+
+  cout << num << "\n\n";
+
+  return 0;
+}
